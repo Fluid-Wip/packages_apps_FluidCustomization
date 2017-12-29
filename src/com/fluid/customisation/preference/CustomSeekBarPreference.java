be0abeea056b6e0bfa2d4b78d8bc8396bc41d616
Pranav Vashi <neobuddy89@gmail.com>
2017-12-30 00:47:07 +0530

    CustomSeekbarPreference: Set progress when setting value
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/fluid/customisation/preference/CustomSeekBarPreference.java
+++ src/com/fluid/customisation/preference/CustomSeekBarPreference.java
@@ -167,6 +167,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
 
     public void setValue(int value) {
         mCurrentValue = value;
+        mSeekBar.setProgress(mCurrentValue - mMin);
     }
 
     @Override
